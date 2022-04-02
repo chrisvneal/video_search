7024454bb149950adfc9831c39d02d2a51e90e85
Chris Neal <chrisvneal@gmail.com>
2022-04-02 04:48:31 -1000

    Install Axios dependency

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyAHsCyfZGVOQ0TRFp1EHKr4cZSBrduY1XA";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
