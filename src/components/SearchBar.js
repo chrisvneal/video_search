49fb4a2e9940da17b946d4c8e1fe10a2827223ed
Chris Neal <chrisvneal@gmail.com>
2022-04-02 01:02:16 -1000

    Convert search form to controlled form

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,20 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = {
+    term: ""
+  };
   render() {
     return (
       <div className="search-bar ui segment">
         <form className="ui form">
           <div className="field">
             <label>Video Search</label>
-            <input type="text" />
+            <input
+              value={this.state.term}
+              type="text"
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
