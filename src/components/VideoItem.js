80a820802780da0092c97fd8764834c80102d3f9
Chris Neal <chrisvneal@gmail.com>
2022-04-03 03:36:55 -1000

    Add video title and image to VideoItem component

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-const VideoItem = (props) => {
-  return <div>video Item</div>;
+const VideoItem = ({ video }) => {
+  let imageURL = video.snippet.thumbnails.medium.url;
+  return (
+    <div>
+      <img src={imageURL} alt={video.snippet.description} />
+      {video.snippet.title}
+    </div>
+  );
 };
 
 export default VideoItem;
