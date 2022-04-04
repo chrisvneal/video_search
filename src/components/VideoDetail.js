d122fcb29612433eacdf67d6b694b7a7be925989
Chris Neal <chrisvneal@gmail.com>
2022-04-03 21:33:53 -1000

    Include iframe element w/ YouTube link in VideoDetail component.

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -8,8 +8,13 @@ const VideoDetail = ({ video }) => {
   let description = video.snippet.description;
   let title = video.snippet.title;
 
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
   return (
     <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} title={video.snippet.title} />
+      </div>
       <div className="ui segment">
         <h4 className="ui header">{title}</h4>
         <p>{description}</p>
